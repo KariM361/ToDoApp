bf91da75ad36abdf25d4f3396d9b94fda8540ad5
Kari idskov holm Madsen <kama7@elev.techcollege.dk>
2025-09-05 11:24:17 +0200

    commit

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,73 @@
 //#region globals
+const mainContainerSection = document.getElementById('mainContainer');
+let currentData = null;
+
 
 //#endregion
 
+initApp()
+
 //#region Model code
+function getData(){
+    console.log('getData');
+ return JSON.parse(localStorage.getItem('toDoApp_v1')) 
+    
+}
+function saveData(myData){
+    console.log('saveData');
+    let serializedData = JSON.stringify(myData)
+
+    localStorage.setItem('toDoApp_v1', serializedData);
+}
+
+function makeNewData(){
+    console.log('makeNewData');
+// dummy data husk at tømme lister inden deployment
+let newData={
+    darkMode:false,
+   
+    lists:[//dataobjecter
+        {
+            listname: 'liste 1',
+            item:[{
+                name:'item 1', done:false },{name:'item 2', done:true},{name:'item 3', done:true}]
+        },
+ 
+        {
+            listname: 'indkøb',
+            item:[{
+                name:'kød', done:false },{name:'salat', done:true},{name:'sovs', done:false}]
+        }
+]
+    
+}
+
+
+return newData;
+
+}
+
 
 //#endregion
 
 //#region Controller code
+function initApp(){
+    console.log('initApp');
+    
+//hent data
+currentData = getData()//er defineret globals
 
+if (currentData==null) {
+//vi har ikke data
+   currentData=makeNewData()
+   saveData(currentData)
+  
+
+} 
+
+//evaluer data
+
+}
 //#endregion
 
 //#region View

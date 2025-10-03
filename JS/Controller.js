f920982ea4d556cb214d7f024e619fc784241f71
Kari idskov holm Madsen <kama7@elev.techcollege.dk>
2025-10-03 08:21:04 +0200

    new

--- JS/Controller.js
+++ JS/Controller.js
@@ -1,4 +1,12 @@
 //knapper og håndtering af bruger input.
+import {renderList} from "./View.js";
+import{makeListView} from "./View.js";
+import{saveData} from "./Model.js";
+import{getData} from "./Controller.js";
+import{makeNewData} from "./Model.js";
+
+
+let currentData = null;
 
 document.getElementById("save") 
 addEventListener("click",()=>{
@@ -14,3 +22,21 @@ let createButton = document.createElement("button")
 createButton.innerText = "MakeNewList"
 // createButton.addEventListener("click", createInput)
 mainContainer.appendChild("MakeNewListButton")
+
+export function initApp(){
+    console.log('initApp');
+    console.log(getData());
+    
+//hent data
+currentData = getData()//er defineret i globals
+
+//evaluer data
+if (currentData==null) {
+//vi har ikke data
+   currentData=makeNewData()
+   saveData(currentData)
+  } 
+
+//vi har data
+makeListView(currentData)
+}
--- JS/Model.js
+++ JS/Model.js
@@ -3,3 +3,24 @@
 let objData = getData()
 let obj ={}
 
+//#region Model code
+export function getData(){
+    console.log('getData');
+    let data=localStorage.getItem('toDoApp_v1')
+    return JSON.parse(data)
+   }
+export function saveData(myData){
+    console.log(myData);
+    let data = getData();
+    if (!data) data = makeNewData();
+    data.lists.push(myData);
+    localStorage.setItem('toDoApp_v1', JSON.stringify(data));
+}
+export function makeNewData(){
+    console.log('makeNewData');
+    return {
+        darkMode: false,
+        lists: []
+    };
+}
+//#endregion model code
